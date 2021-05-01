6ef4f8149c8c64699d27dee48b4a4776f51a45f2
Huy Tran <htt5059@psu.edu>
2021-05-01 01:36:44 -0400

    ...

--- ClientSide/src/Facility/MeetingRoom.java
+++ ClientSide/src/Facility/MeetingRoom.java
@@ -1,24 +1,42 @@
 package Facility;
 
+import Company_People.Department_People.DeptPeopleABS;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class MeetingRoom {
+public class MeetingRoom implements Serializable {
     private int roomNumber;
     private int roomSize;
-    private ArrayList<String> operatingTime = new ArrayList();
+    private DeptPeopleABS user;
+    private boolean isUsed= false;
 
     public MeetingRoom(int roomSize, int number){
         this.roomSize= roomSize;
         roomNumber = number;
     }
 
-    public boolean bookRoom(String time){
-        if(operatingTime.contains(time)){
-            return false;
-        }
-        return true;
+    public boolean bookRoom(DeptPeopleABS user){
+        this.user= user;
+        return isUsed =true;
     }
-    public void done(String time){
-        operatingTime.remove(time);
+    public void done(){
+        isUsed=false;
+        user= null;
+        MeetingRoomManager.getRoomManager().removeFromQ(this);
     }
+    public int roomNum(){return roomNumber;}
+
+    public String toString(){
+        if (user==null)
+            return "Room Number: "+roomNumber;
+        else return "Room Number: "+roomNumber+"    "+user.toString();
+    }
+    public boolean scheduleBefore(MeetingRoom r){
+        return isUsed;
+    }
+    public String getUserInfo(){
+        return user.toString();
+    }
+    public boolean IsUsed(){return isUsed;}
 }
